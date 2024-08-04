2b4711029d2fc35d25b5bd63f9d61fd1746d1fab
Ailwei <61516275+Ailwei@users.noreply.github.com>
2024-08-04 22:38:35 +0200

    updated muleter middleware to temprarly save files o memory

--- server/middleware/multerConfig.js
+++ server/middleware/multerConfig.js
@@ -1,29 +1,15 @@
 const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 
-const uploadDir = path.join(__dirname, '../uploads');
-
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage,
   fileFilter: function (req, file, cb) {
+    
     cb(null, true); 
   },
   limits: {
-    fileSize: 1 * 1024 * 1024 * 1024,
+    fileSize: 1 * 1024 * 1024 * 1024, 
   },
 });
 
