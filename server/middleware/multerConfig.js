19ed8ca63cf37b8c705c327896e2d820c7e7f34b
Ailwei <61516275+Ailwei@users.noreply.github.com>
2024-08-06 18:07:15 +0200

    added supported file types on multer

--- server/middleware/multerConfig.js
+++ server/middleware/multerConfig.js
@@ -1,15 +1,23 @@
 const multer = require('multer');
+const path = require('path');
 
 const storage = multer.memoryStorage();
 
 const upload = multer({
   storage,
   fileFilter: function (req, file, cb) {
-    
-    cb(null, true); 
+    const allowedTypes = /pdf|docx|xlsx|csv|png|jpeg|mp3|mp4|mov|mkv|avi|wmv|ogv|webm/;
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedTypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+      return cb(null, true);
+    } else {
+      cb(new Error('Unsupported file type'), false);
+    }
   },
   limits: {
-    fileSize: 1 * 1024 * 1024 * 1024, 
+    fileSize: 1 * 1024 * 1024 * 1024,
   },
 });
 
