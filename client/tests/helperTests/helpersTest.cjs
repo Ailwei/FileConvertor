904751f54114803a3d49d622e5239173a520dc13
Ailwei <ailweimaemu@gmail.com>
2025-11-02 19:50:28 +0200

    updated the hepertest.cjs

--- client/tests/helperTests/helpersTest.cjs
+++ client/tests/helperTests/helpersTest.cjs
@@ -3,26 +3,38 @@ const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
 
 async function buildDriver() {
+  const options = new chrome.Options()
+    .addArguments('--headless=new')
+    .addArguments('--no-sandbox')
+    .addArguments('--disable-dev-shm-usage')
+    .addArguments('--disable-gpu')
+    .addArguments('--remote-debugging-port=9222')
+    .addArguments(`--user-data-dir=/tmp/chrome-${Date.now()}`);
+
+  const service = new chrome.ServiceBuilder(chromedriver.path).build();
+  chrome.setDefaultService(service);
+
   return new Builder()
     .forBrowser('chrome')
-    .setChromeService(new chrome.ServiceBuilder(chromedriver.path))
+    .setChromeOptions(options)
     .build();
 }
-async function findElement(driver, locator){
-  const element = await driver.wait(until.elementLocated(locator), 5000);
-  await driver.wait(until.elementIsVisible(element), 5000);
+
+async function findElement(driver, locator) {
+  const element = await driver.wait(until.elementLocated(locator), 10000);
+  await driver.wait(until.elementIsVisible(element), 10000);
   return element;
 }
+
 async function click(driver, target) {
   let element;
   if (typeof target === 'object' && typeof target.click === 'function') {
     element = target;
   } else {
-    element = await driver.wait(until.elementLocated(target), 5000);
-    await driver.wait(until.elementIsVisible(element), 5000);
+    element = await driver.wait(until.elementLocated(target), 10000);
+    await driver.wait(until.elementIsVisible(element), 10000);
   }
   await element.click();
 }
 
-
-module.exports = { buildDriver, findElement, click};
+module.exports = { buildDriver, findElement, click };
