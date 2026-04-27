b087edb9c0b9b244d1e88155f104c907fa645898
Ailwei Maemu <Ailweimaemu@gmail.com>
2026-04-27 20:41:02 +0200

    updated the test file to use the correct chrome driver verion

--- client/tests/helperTests/helpersTest.cjs
+++ client/tests/helperTests/helpersTest.cjs
@@ -2,23 +2,22 @@ const { Builder, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
 
-async function buildDriver() {
-  const userDataDir = `/tmp/chrome-${Date.now()}`;
-  const options = new chrome.Options()
-    .addArguments('--headless=new')         
-    .addArguments('--no-sandbox')             
-    .addArguments('--disable-dev-shm-usage')
-    .addArguments('--disable-gpu')
-    .addArguments(`--user-data-dir=${userDataDir}`);
+const { Builder, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
-  const service = new chrome.ServiceBuilder(chromedriver.path);
-   return new Builder()
+async function buildDriver() {
+  const options = new chrome.Options()
+    .addArguments('--headless=new')
+    .addArguments('--no-sandbox')
+    .addArguments('--disable-dev-shm-usage')
+    .addArguments('--disable-gpu');
+
+  return new Builder()
     .forBrowser('chrome')
     .setChromeOptions(options)
-    .setChromeService(service)
     .build();
-
 }
+
 async function findElement(driver, locator){
   const element = await driver.wait(until.elementLocated(locator), 5000);
   await driver.wait(until.elementIsVisible(element), 5000);
